c82872998cb6d1b28fee91b42c3560b169599dc5
Daves Martins <davesmartins@daves.com>
2026-05-21 22:17:12 -0300

    aula 21-05: Api GateWay

---
+++ MS-Biblioteca/GatewayTec/src/main/java/br/uni/ms/libtec/GatawayTec/GatawayTecApplication.java
@@ -0,0 +1,15 @@
+package br.uni.ms.libtec.GatawayTec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatawayTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatawayTecApplication.class, args);
+	}
+
+}
---
+++ MS-Biblioteca/GatewayTec/src/test/java/br/uni/ms/libtec/GatawayTec/GatawayTecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uni.ms.libtec.GatawayTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatawayTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
